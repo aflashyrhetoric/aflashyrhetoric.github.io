b4b2a03fb66afda4f4fb70d236c7dcbcda20d2fc
Kevin Oh <aflashyrhetoric@gmail.com>
2017-04-30 01:44:22 -0400

    Quickcommit command used.

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,8 @@ git worktree add -B gh-pages public origin/gh-pages
 echo "Removing existing files"
 rm -rf public/*
 
-echo "Generating site"
-hugo
+echo "Running yarn build"
+yarn build
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
