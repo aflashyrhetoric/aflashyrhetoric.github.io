f1b6da01c436b3369dbcfb1c433959a65cf70ef5
Kevin Oh <kevin@nulab-inc.com>
2017-04-30 11:45:03 -0400

    Updated script again

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-echo "Deployment has been scheduled."
+echo "*** Deployment has been scheduled."
 
 if [[ $(git status -s) ]]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
+    echo "*** The working directory is dirty. Please commit any pending changes."
     exit 1;
 fi
 
-echo "Deleting old publication"
+echo "*** Deleting old publication"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/master
+echo "*** Checking out master branch into public"
+git worktree add -B master public origin/master
 
-echo "Removing existing files"
+echo "*** Removing existing files"
 rm -rf public/*
 
-echo "Running yarn build"
+echo "*** Running yarn build"
 yarn build
 
-echo "Adding CNAME"
+echo "*** Adding CNAME"
 echo kevinoh.me >> CNAME
 
-echo "Updating gh-pages branch"
+echo "*** Updating master branch with new output"
 cd public && git add -A && git commit -m "Publishing to master (publish.sh)"
 git push
 
