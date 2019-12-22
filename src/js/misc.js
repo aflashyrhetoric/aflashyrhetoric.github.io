2b8f192bc7de3fa4bf66fba0aec3f13254db746b
Kevin Oh <aflashyrhetoric@gmail.com>
2019-12-21 23:28:08 -0500

    Updated the script for randomization to use a linear gradient

--- src/js/misc.js
+++ src/js/misc.js
@@ -8,7 +8,9 @@ const fills = document.querySelectorAll('.fill')
 
 function colorizeAll() {
   const c = randomColor()
-  document.body.style.backgroundColor = c
+  const c2 = randomColor()
+  document.body.style.background = `linear-gradient(to bottom right, ${c}, ${c2})`
+  // button.style.borderImage = `linear-gradient(to bottom right, ${c} 0%, ${c2} 25%, ${randomColor()} 50%, ${randomColor()} 75%, ${randomColor()} 100%)`
   hero.style.backgroundColor = c
   button.style.color = c
 }
@@ -36,6 +38,5 @@ function randomColor() {
   const hue = getRandomInt(360)
   const saturation = 75
   const luminosity = 75
-
   return hsl(hue, saturation, luminosity)
 }
