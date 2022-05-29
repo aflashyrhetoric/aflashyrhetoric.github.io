fccc7c3ee1f354ae2fb586a3a3ae13a161251cde
Kevin Oh <aflashyrhetoric@gmail.com>
2022-05-29 14:20:26 -0400

    Update styling misc - update content misc

--- src/js/colorize.js
+++ src/js/colorize.js
@@ -1,27 +1,29 @@
-const hsl = require('hsl-to-hex') // import the script
+const hsl = require("hsl-to-hex"); // import the script
 
-const hero = document.querySelector('.upper-text__wrapper')
-const button = document.querySelector('.js_color-button')
-const els = [document.body, hero, button]
+const hero = document.querySelector(".upper-text__wrapper");
+const button = document.querySelector(".js_color-button");
+const highlightBar = document.querySelector(".highlight-bar");
+const els = [document.body, hero, button];
 
-const fills = document.querySelectorAll('.fill')
+const fills = document.querySelectorAll(".fill");
 
 export function colorizeAll() {
-  const c = randomColor()
-  const c2 = randomColor()
-  document.body.style.background = `linear-gradient(to bottom right, ${c}, ${c2})`
+  const c = randomColor();
+  const c2 = randomColor();
+  document.body.style.background = `linear-gradient(to bottom right, ${c}, ${c2})`;
   // button.style.borderImage = `linear-gradient(to bottom right, ${c} 0%, ${c2} 25%, ${randomColor()} 50%, ${randomColor()} 75%, ${randomColor()} 100%)`
-  hero.style.backgroundColor = c
-  button.style.color = c
+  hero.style.backgroundColor = c;
+  button.style.color = c;
+  highlightBar.style.opacity = 1;
 }
 
-const c = randomColor()
+const c = randomColor();
 // document.body.style.backgroundColor = c
 // hero.style.backgroundColor = c
-button.style.color = c
+button.style.color = c;
 
-hero.addEventListener('click', colorizeAll)
-button.addEventListener('click', colorizeAll)
+hero.addEventListener("click", colorizeAll);
+button.addEventListener("click", colorizeAll);
 
 // fills.forEach((f) => {
 //   const c = randomColor()
@@ -31,12 +33,12 @@ button.addEventListener('click', colorizeAll)
 // button.textContent =.split.forEach(letter => letter.style.)
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max))
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 export function randomColor() {
-  const hue = getRandomInt(360)
-  const saturation = 75
-  const luminosity = 75
-  return hsl(hue, saturation, luminosity)
+  const hue = getRandomInt(360);
+  const saturation = 75;
+  const luminosity = 75;
+  return hsl(hue, saturation, luminosity);
 }
